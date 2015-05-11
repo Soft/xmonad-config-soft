567ea14de3afd088f26d2bc52dcf92a157dc7415
Samuel Laur?n <samuel.lauren@iki.fi>
2015-05-11 17:25:34 +0300

    Changed binding

--- XMonad/Config/Soft/Bindings.hs
+++ XMonad/Config/Soft/Bindings.hs
@@ -31,7 +31,7 @@ keyMap xc = mkKeymap xc
   , ("M-x", rofi internalPrompt)
   , ("M-c", withFocused centerWindow)
 
-  , ("M-t", sendMessage ToggleSpacing)
+  , ("M-n", sendMessage ToggleSpacing)
 
   -- Keybindings for BinarySpacePartition
   , ("C-M-l", sendMessage $ MoveSplit R)
@@ -59,5 +59,4 @@ launchers =
   , (xK_p, "pcmanfm", [],                     className =? "Pcmanfm")
   , (xK_t, "lxtask", [],                      className =? "Lxtask")
   , (xK_a, "urxvt", ["-e", "tmux", "attach"], title =? "tmux")]
-
 
